bce893fe46551705c1406e391a42430e5cd3facf
JatinRout <jatinrout@localhost.local>
2017-09-09 02:23:28 +0530

    News-Feed-commit22

--- analytics.py
+++ analytics.py
@@ -34,7 +34,7 @@ def analytics_check():
         headlines=','.join(str(line.encode('utf-8')) for line in summaries)
     with document(title='Analytics') as doc:
         h1('News Summary')
-        print headlines
+        h2(headlines)
     
     with open('templates/analytics.html', 'w') as f:
         f.write(doc.render())
