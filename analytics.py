ac2df89c2f12a5aa41b0294a6c6a377270ba542c
JatinRout <jatinrout@localhost.local>
2017-09-09 23:34:26 +0530

    Added changes20

--- analytics.py
+++ analytics.py
@@ -49,8 +49,7 @@ def analytics_check():
     for url in urls:
         page = urllib2.urlopen(url).read().decode('utf8')
         soup = BeautifulSoup(page)
-        text = ' '.join(map(lambda p: p.text.encode('ascii', 'ignore'), soup.find_all('p')))
-        print 'Text is %s'%(text)
+        content = ' '.join(map(lambda p: p.text.encode('ascii', 'ignore'), soup.find_all('p')))
         #headlines='\n'.join(str(line.encode('ascii', 'ignore')) for line in summaries)
         sentences_dic = fs.get_senteces_ranks(text)
         headlines=fs.get_summary(title, content, sentences_dic)
